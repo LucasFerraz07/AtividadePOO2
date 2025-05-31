b2f1253bca2b5b91ecd79b1f8a9ffc69fefdaf4d
LucasFerraz07 <lucas.ferrazm0411@gmail.com>
2025-05-31 20:17:52 -0300

    exc002 concluido

---
+++ exc002/main.py
@@ -0,0 +1,8 @@
+from FuncionarioCLT import FuncionarioCLT
+from FuncionarioPJ import FuncionarioPJ
+
+t1 = FuncionarioCLT("Lucas", 2300)
+t2 = FuncionarioPJ("Letícia", 32, 50)
+
+t1.calcular_pagamento()
+t2.calcular_pagamento()
